91397c80d6502599cd5ab4a2583a5f13a7ebda54
kriswep <kriswep@wetainment.com>
2022-06-12 23:50:34 +0200

    scan qr codes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
 import Head from 'next/head';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+let barcodeDetector;
+const getBarcodeDetector = async () => {
+  if (barcodeDetector) return barcodeDetector;
+  if (typeof window !== 'undefined') {
+    // browser code
+    if (!('BarcodeDetector' in window)) {
+      const BarcodeDetector = await import('barcode-detector');
+      window.BarcodeDetector = BarcodeDetector.default;
+    }
+    barcodeDetector = new window.BarcodeDetector({
+      formats: ['qr_code'],
+    });
+    return barcodeDetector;
+  }
+  return barcodeDetector;
+};
 
 export default function Home() {
   const videoRef = useRef(null);
@@ -7,6 +24,35 @@ export default function Home() {
   useEffect(() => {
     getVideo();
   }, [videoRef]);
+
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    let interval;
+    // const barcodeDetector = new BarcodeDetector({ formats: ['qr_code'] });
+    const scanCodes = async () => {
+      const barcodeDetector = await getBarcodeDetector();
+      if (!barcodeDetector || !videoRef.current) {
+        return;
+      }
+
+      interval = setInterval(async () => {
+        const barcodes = await barcodeDetector.detect(videoRef.current);
+        if (!barcodes || barcodes.length <= 0) {
+          setText('no barcode detected');
+          return;
+        }
+        console.log(barcodes);
+        setText(barcodes.map((barcode) => barcode.rawValue));
+      }, 300);
+
+      return () => {
+        if (interval) return clearInterval(interval);
+      };
+    };
+
+    scanCodes().catch(console.error);
+  }, [videoRef, setText]);
 
   const getVideo = () => {
     navigator.mediaDevices
@@ -23,10 +69,10 @@ export default function Home() {
 
   return (
     <div className="flex h-screen w-screen justify-around flex-col">
-      <h1 className="text-3xl font-bold text-gray-200 self-center">
-        Hello ArQr
-      </h1>
-      <video ref={videoRef} className="w-screen min-h-min" />
+      <video ref={videoRef} className="w-screen h-screen" />
+      <p className="text-6xl font-bold text-gray-200 absolute left-5 top-5">
+        {text}
+      </p>
     </div>
   );
 }
